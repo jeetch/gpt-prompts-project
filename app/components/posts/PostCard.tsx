9cb9b9f6655035c6c6ed7486db8dd737f0b0adde
jeetch <jeetch96@gmail.com>
2023-04-17 13:25:38 +0530

    category header to catgory page

--- app/components/posts/PostCard.tsx
+++ app/components/posts/PostCard.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useCallback, useMemo, useState } from "react";
 import { SafePost, SafeUser } from "@/app/types";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import HeartButton from "../HeartButton";
 import Button from "../Button";
 import { ButtonUi } from "../ButtonUi";
@@ -10,6 +10,7 @@ import { IoCopy } from "react-icons/io5";
 import { toast } from "react-hot-toast";
 import { SiOpenai } from "react-icons/si";
 import { categories } from "../navbar/Categories";
+import qs from "query-string";
 
 interface PostCardProps {
   data: SafePost & {
@@ -84,25 +85,57 @@ const PostCard: FC<PostCardProps> = ({
     return category ? category.icon : "";
   };
 
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: data.category,
+    };
+
+    if (params?.get("category") == data.category) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [data.category, params, router]);
+
   return (
     <div className="relative flex flex-col col-span-1 gap-y-2  group border hover:border-emerald-700 border-emerald-900 rounded-md shadow-md hover:shadow-xl p-4">
       <div className="absolute top-2 right-2">
         <HeartButton postId={data.id} currentUser={currentUser} />
       </div>
 
-      <span className=" text-emerald-400 text-xs font-medium ">
+      <span
+        className=" text-emerald-400 text-xs  cursor-pointer  hover:font-semibold"
+        onClick={handleClick}
+      >
         {getCategoryIcon(data.category)} {data.category}
       </span>
 
       <div
-        className="font-semibold text-neutral-200 text-lg cursor-pointer hover:text-highlight-400"
+        className="font-semibold text-neutral-200 text-lg cursor-pointer hover:text-highlight-400 hover:font-bold"
         onClick={() => router.push(`/posts/${data.id}`)}
       >
         {data.title}
       </div>
       <textarea
         className="select-all scrollbar-thin selection:bg-emerald-600/30 scrollbar-thumb-emerald-700 scrollbar-track-sky-900 font-light text-sm font-mono text-neutral-200 w-full bg-sky-900 border
-     border-emerald-900 p-2 rounded-md cursor-pointer hover:shadow-md h-[200px] focus:outline-none focus:ring focus:ring-emerald-600"
+     border-emerald-900 p-2 rounded-md cursor-pointer hover:border-emerald-700 hover:shadow-md h-[200px] focus:outline-none focus:ring focus:ring-emerald-600"
         onClick={() => {}}
       >
         {data.prompt}
