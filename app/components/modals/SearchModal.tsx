69bf43ca075bbb3793b14731c7dda192b856b4b9
jeetch <jeetch96@gmail.com>
2023-04-17 20:35:07 +0530

    working on searc

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -13,6 +13,7 @@ import { formatISO } from "date-fns";
 import { RouteHandlerManager } from "next/dist/server/future/route-handler-managers/route-handler-manager";
 import Heading from "../Heading";
 import Counter from "../inputs/Counter";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../Tabs";
 
 interface SearchModalProps {}
 
@@ -59,6 +60,53 @@ const SearchModal: FC<SearchModalProps> = ({}) => {
         title="What are you looking for?"
         subtitle="Find the perfect prompt"
       />
+
+      <Tabs defaultValue="category" className="w-full">
+        <TabsList>
+          <TabsTrigger value="category">Search by category</TabsTrigger>
+          <TabsTrigger value="text">Search by text</TabsTrigger>
+        </TabsList>
+        <TabsContent value="category"></TabsContent>
+        <TabsContent value="text">
+          <form>
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 
